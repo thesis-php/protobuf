737357a68fd42ad0011b8ed6906e642ec5f7d70d
kafkiansky <vadimzanfir@gmail.com>
2025-12-09 16:29:35 +0300

    chore: implement google/protobuf/empty

---
+++ src/Known/EmptyObject.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Protobuf\Known;
+
+/**
+ * @api
+ * @see https://github.com/protocolbuffers/protobuf/blob/main/src/google/protobuf/empty.proto
+ */
+final readonly class EmptyObject {}
