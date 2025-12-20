621ad6dfcf330106b43b3b795c2c85c8067b3bbe
kafkiansky <vadimzanfir@gmail.com>
2025-12-20 16:17:51 +0300

    chore: file package can be null

--- src/Compiler/FileDescriptorProto.php
+++ src/Compiler/FileDescriptorProto.php
@@ -26,7 +26,7 @@ final readonly class FileDescriptorProto
         #[Reflection\Field(1, Reflection\StringT::T)]
         public string $name,
         #[Reflection\Field(2, Reflection\StringT::T)]
-        public string $package,
+        public ?string $package = null,
         #[Reflection\Field(3, new Reflection\ListT(Reflection\StringT::T))]
         public array $dependencies = [],
         #[Reflection\Field(10, new Reflection\ListT(Reflection\Int32T::T))]
